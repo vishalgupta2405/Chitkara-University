a2ada1ab1e2697b306e86da540fa7af378f4b6bd
Vishal Gupta <54734535+vishalgupta2405@users.noreply.github.com>
2021-01-29 00:02:00 +0530

    Add files via upload

---
+++ format.js
@@ -0,0 +1,18 @@
+function f1()
+{
+   var c=document.getElementsByTagName("p")[0];
+  var d=document.getElementsByTagName("p")[1];
+  var e=document.getElementsByTagName("p")[2];
+  var f=document.getElementsByTagName("p")[3];
+  
+       c.style.backgroundColor="orange";
+      d.style.backgroundColor="red";
+     e.style.backgroundColor="blue";
+      f.style.backgroundColor="grey";
+     
+}
+
+
+
+
+
